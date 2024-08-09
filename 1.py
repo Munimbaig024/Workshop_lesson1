bf4952b3d26863530e2ce260ed37852f116e7a62
Munimbaig024 <114643081+Munimbaig024@users.noreply.github.com>
2024-08-09 11:12:35 +0500

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,11 @@
+# git init to activate 
+# print ('HEY , YOU ! ')
+
+def add (a , b ):
+    return a+ b 
+
+
+if __name__ == "__main__":
+    
+    result = add (5 , 5 )
+    print ( f"Result : " , result) ; 
