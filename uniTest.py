6599d6d9d1c4cd469e2e68c288833a1f831cd955
MunimBaig024 <munimbaig12345@gmail.com>
2024-08-09 11:37:08 +0500

    Minor Change

---
+++ uniTest.py
@@ -0,0 +1,17 @@
+import pytest
+# import uniTest 
+from Calculator import Add
+
+
+def add (a , b):
+       return a + b ; 
+
+def test_add():
+    assert add(1, 2) == 3 ; 
+    
+    
+    
+    
+    
+    
+    
